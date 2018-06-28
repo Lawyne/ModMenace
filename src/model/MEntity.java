f2666493c666463dbc7ad66caea02ca10a6e8bb2
sunti <tithnara.sun@ensta-bretagne.org>
2018-06-28 10:36:06 +0200

    Comments

--- src/model/MEntity.java
+++ src/model/MEntity.java
@@ -4,27 +4,34 @@ import java.util.LinkedList;
 
 public class MEntity extends MObject{
     protected String name;
-    protected LinkedList<MLink> outs;
-
-
+    protected LinkedList<MLink> outs; //lists of exiting links
 
     public MEntity(String name) {
         this.name=name;
         this.outs= new LinkedList<MLink>();
     }
 
+    //return name
     public String getName() {
         return name;
     }
 
+    //set name
     public void setName(String name) {
         this.name = name;
     }
 
+    //adds link to the outs
     public void addLink(MLink link){
         outs.add(link);
     }
 
+    //removes link from the outs
+    public void remove(MLink link) {
+        outs.remove(link);
+    }
+
+    //returns outs
     public LinkedList<MLink> getOuts() {
         return outs;
     }
